b76abd53120a14c2e1c2f0ab5c1a4e0e88a53e6e
Isira Samaraweera <isirakasun@gmail.com>
2024-09-05 15:32:02 +0530

    Icons and descriptions changed

--- src/Components/Home/Type.jsx
+++ src/Components/Home/Type.jsx
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
+          "Software Engineer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "MERN Stack Developer"
         ],
         autoStart: true,
         loop: true,

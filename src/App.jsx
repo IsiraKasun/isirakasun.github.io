09205c75f3240a52417495fbdcd15c48ba7d5263
Isira Samaraweera <isirakasun@gmail.com>
2024-12-03 08:12:38 +0530

    App.jsx Pre component resolution failed issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
+import Preloader from "./components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./components/About/About";
